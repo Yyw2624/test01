758600e23ae88e95ba7e07e3b1e02f5565fb36ae
yyw <ywook303@gmail.com>
2024-05-17 23:39:11 +0900

    first  commit

---
+++ src/FourArithemetic.java
@@ -0,0 +1,17 @@
+public class FourArithemetic {
+    public static void main(String[] args)
+    {
+        int a,b, sum, cha, goeb, nanu;
+        a = 12;
+        b = 2;
+        sum = a + b;
+        cha = a-b;
+        goeb = a*b;
+        nanu = a/b;
+
+        System.out.println("a+b=" + sum);
+        System.out.println(cha);
+        System.out.println(goeb);
+        System.out.println(nanu);
+    }
+}
---
+++ src/Operator.java
@@ -0,0 +1,14 @@
+public class Operator
+{
+    public static void main(String[] args)
+    {
+        int kor,eng, tot, avr;
+        kor = 77;
+        eng = 87;
+        tot = kor + eng;
+        avr = tot/2;
+
+        System.out.println("총점:" + tot);
+        System.out.println("평균:" + avr);
+    }
+}
